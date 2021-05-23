2962c8f9d5d149c9a9af03c226ca63bab285ceb8
pawandaviet7 <pawandaviet7@gmail.com>
2021-05-23 16:34:35 +0530

    Property injection using p Schema and using value as attribute

--- src/main/java/com/pk/springcore/App.java
+++ src/main/java/com/pk/springcore/App.java
@@ -15,6 +15,13 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         Student student = (Student) context.getBean("student");
         System.out.println(student);
+        Student student2 = (Student) context.getBean("student2");
+        System.out.println(student2);
+        Student student3 = (Student) context.getBean("student3");
+        System.out.println(student3);
+        Student student4 = (Student) context.getBean("student4");
+        System.out.println(student4);
+
 
     }
 }
