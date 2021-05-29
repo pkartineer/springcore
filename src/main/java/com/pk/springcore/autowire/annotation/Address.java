c651df1b121b6ee754b503bf38f8cc96624a70c0
pawandaviet7 <pawandaviet7@gmail.com>
2021-05-29 15:21:29 +0530

    @Autowired Annotation for Autowiring

---
+++ src/main/java/com/pk/springcore/autowire/annotation/Address.java
@@ -0,0 +1,31 @@
+package com.pk.springcore.autowire.annotation;
+
+public class Address {
+
+    private String street;
+    private String city;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
