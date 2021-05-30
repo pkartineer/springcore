b70e35120e87a85649564c0718e7cd6ae650580f
pawandaviet7 <pawandaviet7@gmail.com>
2021-05-30 19:35:01 +0530

    Spring Bean Scope | Singleton | Prototype

--- src/main/java/com/pk/springcore/stereotype/Test.java
+++ src/main/java/com/pk/springcore/stereotype/Test.java
@@ -12,5 +12,17 @@ public class Test {
         Student student =  applicationContext.getBean("stu", Student.class);
 
         System.out.println(student);
+        System.out.println(student.hashCode());
+        System.out.println("==============");
+        Student student2 =  applicationContext.getBean("stu", Student.class);
+        System.out.println(student2.hashCode());
+
+        System.out.println("======= TEACHER =======");
+        Teacher teacher =  applicationContext.getBean("teacher", Teacher.class);
+        System.out.println(teacher.hashCode());
+        System.out.println("==============");
+        Teacher teacher2 =  applicationContext.getBean("teacher", Teacher.class);
+        System.out.println(teacher2.hashCode());
+
     }
 }
