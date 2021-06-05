49a1b8073dfe218da074e045701cceec201738a7
pawandaviet7 <pawandaviet7@gmail.com>
2021-06-05 12:15:34 +0530

    How to invoke static method , variables and creating object

--- src/main/java/com/pk/springcore/spel/Demo.java
+++ src/main/java/com/pk/springcore/spel/Demo.java
@@ -13,12 +13,52 @@ public class Demo {
     @Value("#{323+2323+2323+232}")
     private int y;
 
+    //calling static method
+    @Value("#{T(java.lang.Math).sqrt(625)}")
+    private int z;
+
+    //calling static variable
+    @Value("#{T(java.lang.Math).PI}")
+    private double d;
+
+    //Calling constructor. You can call any of class constructor like this
+    @Value("#{ new java.lang.String('Pawan Kumar')}")
+    private String name;
+
+
     @Override
     public String toString() {
         return "Demo{" +
                 "x=" + x +
                 ", y=" + y +
+                ", z=" + z +
+                ", d=" + d +
+                ", name='" + name + '\'' +
                 '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
     }
 
     public int getX() {
